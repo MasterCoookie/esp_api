77446674da33e9120c29a5a427b2a2a782358e2c
MasterCoookie <jas.kocurek2@wp.pl>
2022-07-08 11:49:56 +0200

    adds mongoose db connection

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const routes = require('./routes');
-
+const mongoose = require('mongoose');
+const db_pwd  = require('./db_pwd');
 
 const app = express();
 app.set('view engine', 'ejs');
-
 const port = 8080;
-
 app.use(routes);
 
-app.listen(port);
+const db = 'mongodb+srv://esp_api_client:' + db_pwd + '@espproject.ok0kk.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(db, {}).then(result => {
+    console.log('DB connection established');
+    app.listen(port);
+    console.log("Listening for requests on port %s...", port);
+}).catch(err => { console.log(err); });
 
-console.log("Listening for requests on port %s...", port);
+
+
+
+
