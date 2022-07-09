6c12eef6d484f236072d2080b180bb41d1b3ae02
MasterCoookie <jas.kocurek2@wp.pl>
2022-07-09 17:34:34 +0200

    device model created

--- models/deviceModel.js
+++ models/deviceModel.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const deviceSchema = new Schema({
+    owners: [Schema.Types.ObjectId],
+    name: String,
+});
+
+const Device = mongoose.model('model', deviceSchema);
+
+module.exports = Device;
