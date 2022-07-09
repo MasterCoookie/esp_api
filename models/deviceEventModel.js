6d9e6a27bf2b5e9238685b2f977b29c689aab830
MasterCoookie <jas.kocurek2@wp.pl>
2022-07-09 20:01:18 +0200

    creates deviceEvent Schema and model

---
+++ models/deviceEventModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const deviceEventSchema = new Schema({
+    deviceID: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    //TOOD: check for colliding events
+    eventTime: Date,
+    targetYpos: Number,
+}, { timestamps: true });
+
+const deviceEvent = mongoose.model('deviceEvent', deviceEventSchema);
+
+module.exports = deviceEvent;
