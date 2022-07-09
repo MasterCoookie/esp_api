f9f5042af0fdd938c19323dcf4a12ec4f0ae4670
MasterCoookie <jas.kocurek2@wp.pl>
2022-07-09 20:53:03 +0200

    adds regiser_device controller

--- controller.js
+++ controller.js
@@ -1,4 +1,5 @@
 const User = require('./models/userModel');
+const Device = require('./models/deviceModel');
 
 const index_get =  (req, res) => {
     console.log("New request");
@@ -46,4 +47,18 @@ const random_test = (req, res) => {
     res.status(200).json({ dupa: "dupa" });
 }
 
-module.exports =  { index_get, signup_post, user_check, random_test };
+const regiser_device = async (req, res) => {
+    const { ownerID, name } = req.body;
+    
+    try {
+        const registeredDevice = await Device.create({ owners: [ownerID] ,name });
+        console.log("New device %s registered by %s", ownerID, name);
+        await User.findByIdAndUpdate(ownerID , { "$push": { devicesList: registeredDevice._id } });
+        res.status(201).json({ success: true })
+    } catch(err) {
+        const errors = errorHandler(err);
+        res.status(400).json({ success: false });
+    }
+}
+
+module.exports =  { index_get, signup_post, user_check, random_test, regiser_device };
--- routes.js
+++ routes.js
@@ -10,5 +10,6 @@ router.get('/', controller.index_get);
 router.post('/user_create', controller.signup_post)
 router.post('/user_check', controller.user_check);
 router.post('/random_test', authMiddleware.requireAuth, controller.random_test);
+router.post('/register_device', authMiddleware.requireAuth, controller.regiser_device);
 
 module.exports = router;
