6e0b2b201412776d73f1d117290437009cf5ab67
MasterCoookie <jas.kocurek2@wp.pl>
2022-08-01 20:44:17 +0200

    deviceSchema device setup fields added

--- models/deviceModel.js
+++ models/deviceModel.js
@@ -5,6 +5,9 @@ const deviceSchema = new Schema({
     owners: [Schema.Types.ObjectId],
     MAC: String,
     name: String,
+    motorSpeed: Number,
+    wifiName: String,
+    wifiPassword: String,
     pendingEventID: {
         type: Schema.Types.ObjectId,
         default: null
