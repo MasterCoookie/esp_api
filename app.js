7e884d433c37d30eb8dd6fd55ce7057b3b347f10
MasterCoookie <jas.kocurek2@wp.pl>
2022-08-14 13:30:22 +0200

    for now, changed back to http

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const db_pwd  = require('./db_pwd');
 const bodyParser = require('body-parser');
 const https = require("https");
+const http = require("http");
 const fs = require('fs');
 
 const app = express();
@@ -18,10 +19,14 @@ app.use(routes);
 const db = 'mongodb+srv://esp_api_client:' + db_pwd + '@espproject.ok0kk.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(db, {}).then(result => {
     console.log('DB connection established');
-    https.createServer({
-        key: fs.readFileSync("key.pem"),
-        cert: fs.readFileSync("cert.pem"),
-      },app).listen(8080, ()=>{
-        console.log('Listening for requests on port 8080...');
-    });
+    // https.createServer({
+    //     key: fs.readFileSync("key.pem"),
+    //     cert: fs.readFileSync("cert.pem"),
+    //   },app).listen(8080, ()=>{
+    //     console.log('Listening for HTTPS requests on port 8080...');
+    // });
+    http.createServer({
+    },app).listen(8080, ()=>{
+      console.log('Listening for HTTP requests on port 8080...');
+  });
 }).catch(err => { console.log(err); });
