8dc80fe4a7d5b880759be969429df9d7cdd71cac
MasterCoookie <jas.kocurek2@wp.pl>
2022-08-14 14:14:26 +0200

    well mow its both

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ app.use(routes);
 const db = 'mongodb+srv://esp_api_client:' + db_pwd + '@espproject.ok0kk.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(db, {}).then(result => {
     console.log('DB connection established');
-    // https.createServer({
-    //     key: fs.readFileSync("key.pem"),
-    //     cert: fs.readFileSync("cert.pem"),
-    //   },app).listen(8080, ()=>{
-    //     console.log('Listening for HTTPS requests on port 8080...');
-    // });
+    https.createServer({
+        key: fs.readFileSync("key.pem"),
+        cert: fs.readFileSync("cert.pem"),
+      },app).listen(8081, ()=>{
+        console.log('Listening for HTTPS requests on port 8081...');
+    });
     http.createServer({
     },app).listen(8080, ()=>{
       console.log('Listening for HTTP requests on port 8080...');
