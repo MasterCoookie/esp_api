3fdcc70df965b9d61a8be6c005e0ac6563281eea
MasterCoookie <jas.kocurek2@wp.pl>
2022-08-21 13:18:16 +0200

    hiraku hosting preps

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const fs = require('fs');
 const app = express();
 app.set('view engine', 'ejs');
 const portHTTPS = 8080;
-const portHTTP = 3000;
+const portHTTP = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,12 +20,12 @@ app.use(routes);
 const db = 'mongodb+srv://esp_api_client:' + db_pwd + '@espproject.ok0kk.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(db, {}).then(result => {
     console.log('DB connection established');
-    https.createServer({
-        key: fs.readFileSync("key.pem"),
-        cert: fs.readFileSync("cert.pem"),
-      },app).listen(portHTTPS, ()=>{
-        console.log('Listening for HTTPS requests on port %i...', portHTTPS);
-    });
+    // https.createServer({
+    //     key: fs.readFileSync("key.pem"),
+    //     cert: fs.readFileSync("cert.pem"),
+    //   },app).listen(portHTTPS, ()=>{
+    //     console.log('Listening for HTTPS requests on port %i...', portHTTPS);
+    // });
     http.createServer({
     },app).listen(portHTTP, ()=>{
       console.log('Listening for HTTP requests on port %i...', portHTTP);
